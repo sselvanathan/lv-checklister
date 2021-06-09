17ef73ef1d0dc7ab345f95d93898bb3345659b25
Senthu <senthuu27@gmail.com>
2021-06-09 10:58:17 +0200

    remove unneccessary doc blocks adjust stub

--- app/Http/Controllers/Admin/ChecklistGroupController.php
+++ app/Http/Controllers/Admin/ChecklistGroupController.php
@@ -5,64 +5,36 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreChecklistGroupRequest;
 use App\Models\ChecklistGroup;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class ChecklistGroupController extends Controller
 {
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
-    public function create()
+    public function create(): View
     {
         return view('admin.checklist_groups.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param StoreChecklistGroupRequest $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function store(StoreChecklistGroupRequest $request): \Illuminate\Http\RedirectResponse
+    public function store(StoreChecklistGroupRequest $request): RedirectResponse
     {
         ChecklistGroup::create($request->validated());
 
         return redirect()->route('home');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param ChecklistGroup $checklistGroup
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
-    public function edit(ChecklistGroup $checklistGroup)
+    public function edit(ChecklistGroup $checklistGroup): View
     {
         return view('admin.checklist_groups.edit', compact('checklistGroup'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param StoreChecklistGroupRequest $request
-     * @param ChecklistGroup $checklistGroup
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function update(StoreChecklistGroupRequest $request, ChecklistGroup $checklistGroup)
+    public function update(StoreChecklistGroupRequest $request, ChecklistGroup $checklistGroup): RedirectResponse
     {
         $checklistGroup->update($request->validated());
 
         return redirect()->route('home');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param ChecklistGroup $checklistGroup
-     * @return \Illuminate\Http\RedirectResponse
-     */
-    public function destroy(ChecklistGroup $checklistGroup)
+    public function destroy(ChecklistGroup $checklistGroup): RedirectResponse
     {
         $checklistGroup->delete();
 
--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -3,82 +3,44 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Page;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(): View
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function create(): View
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
         //
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show(Page $page): View
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function edit(Page $page): View
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(Request $request, Page $page): RedirectResponse
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function destroy(Page $page): RedirectResponse
     {
         //
     }
