e20f1c656d9aaa196d16eef89bbf95d412eb7799
Senthu <senthuu27@gmail.com>
2021-06-09 20:28:00 +0200

    Implement Feature to Manage Pages

---
+++ resources/views/admin/pages/edit.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="fade-in">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+                        <form
+                            action="{{ route('admin.pages.update', [$page]) }}"
+                            method="POST">
+                            @csrf
+                            @method('PUT')
+                            <div class="card-header">{{ __('Edit Page') }}</div>
+
+                            <div class="card-body">
+                                @if (session('message'))
+                                    <div class="alert alert-info">{{ session('message') }}</div>
+                                @endif
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="form-group">
+                                            <label for="title">{{ __('Title') }}</label>
+                                            <input value="{{$page->title}}" class="form-control" name="title"
+                                                   type="text">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="content">{{ __('Content') }}</label>
+                                            <textarea id="page-textarea" class="form-control" name="content">{{$page->content}}</textarea>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button class="btn btn-sm btn-primary" type="submit">{{ __('Save') }}</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+
+@section('scripts')
+    <script>
+        ClassicEditor
+            .create( document.querySelector( '#page-textarea' ) )
+            .catch( error => {
+                console.error( error );
+            } );
+    </script>
+@endsection
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -7,18 +7,6 @@
                 {{__('Dashboard')}}</a></li>
 
         @if(@auth()->user()->is_admin)
-            <li class="c-sidebar-nav-title">{{__('Admin')}}</li>
-            <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
-                <a
-                    class="c-sidebar-nav-link"
-                    href="{{ route('admin.pages.index') }}">
-                    <svg class="c-sidebar-nav-icon">
-                        <use xlink:href="{{ asset('images/svg/coreui/free.svg#cil-puzzle')}}"></use>
-                    </svg>
-                    {{__('Pages')}}
-                </a>
-            </li>
-
             <li class="c-sidebar-nav-title">{{__('Manage Checklists')}}</li>
             @foreach(\App\Models\ChecklistGroup::with('checklists')->get() as $group)
                 <li class="c-sidebar-nav-item c-sidebar-nav-dropdown c-show">
@@ -46,21 +34,23 @@
                 <a class="c-sidebar-nav-link "
                    href="{{ route('admin.checklist_groups.create') }}">{{__('New Checklist Group')}}</a>
             </li>
+
+            <li class="c-sidebar-nav-title">{{__('Pages')}}</li>
+            @foreach(\App\Models\Page::all() as $page)
+                <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
+                    <a
+                        class="c-sidebar-nav-link"
+                        href="{{ route('admin.pages.edit', $page) }}">
+                        <svg class="c-sidebar-nav-icon">
+                            <use xlink:href="{{ asset('images/svg/coreui/free.svg#cil-puzzle')}}"></use>
+                        </svg>
+                        {{ $page->title }}
+                    </a>
+                </li>
+            @endforeach
         @endif
 
-        <li class="c-sidebar-nav-item c-sidebar-nav-dropdown">
-            <a
-                class="c-sidebar-nav-link c-sidebar-nav-dropdown-toggle" href="#">
-                <svg class="c-sidebar-nav-icon">
-                    <use xlink:href="{{ asset('images/svg/coreui/free.svg#cil-puzzle')}}"></use>
-                </svg>
-                {{__('Base')}}</a>
-            <ul class="c-sidebar-nav-dropdown-items">
-                <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="base/breadcrumb.html"><span
-                            class="c-sidebar-nav-icon"></span> {{__('Breadcrumb')}}</a></li>
-            </ul>
-        </li>
-
+        <li class="c-sidebar-nav-title">{{__('Other')}}</li>
         <li class="c-sidebar-nav-item"><a class="c-sidebar-nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                                  document.getElementById('logout-form').submit();">
                 <svg class="c-sidebar-nav-icon">
